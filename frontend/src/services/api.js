9412972c40878b419f5e1dec291c2230a14b0829
Alvaro Franqueira <franqueira.alvaro@gmail.com>
2025-05-22 15:10:44 +0200

    localhost change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://casino-backend-x9cd.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
